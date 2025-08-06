87d46a7328b01f50ca1474cb3a0defae3d2ed4b4
sylv256 <contact@sylv.gay>
2025-08-06 00:47:46 -0400

    chore: Initial commit 2

---
+++ src/main/java/dev/sweetberry/more_than_a_foxbox/MoreThanAFoxbox.java
@@ -0,0 +1,21 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package dev.sweetberry.more_than_a_foxbox;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreThanAFoxbox implements ModInitializer {
+	public static final String ID = "more_than_a_foxbox";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+	@Override
+	public void onInitialize() {
+		//LOGGER.info("[Mod ID] pretty pink princess ponies prancing perpendicular");
+	}
+}
