a8a9f62e7a43d79e3851d70be922951b9a686927
Oliver-makes-code <oliver@olivermakesco.de>
2025-08-07 23:41:00 -0500

    *defers your registry*

--- src/main/java/dev/sweetberry/more_than_a_foxbox/item/MtfbItems.java
+++ src/main/java/dev/sweetberry/more_than_a_foxbox/item/MtfbItems.java
@@ -1,30 +1,33 @@
 package dev.sweetberry.more_than_a_foxbox.item;
 
 import dev.sweetberry.more_than_a_foxbox.MoreThanAFoxbox;
-import net.minecraft.core.Registry;
+import dev.sweetberry.more_than_a_foxbox.registry.RegistryContext;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
 
-import java.util.function.Function;
-
 public class MtfbItems {
-	public static final ResourceKey<Item> SPEAKER = key("speaker");
-	public static final ResourceKey<Item> SQUEAKER = key("squeaker");
-	public static final ResourceKey<Item> CARDBOARD = key("cardboard");
+	private static final RegistryContext<Item, Item.Properties> CONTEXT = new RegistryContext<>(
+		BuiltInRegistries.ITEM,
+		MoreThanAFoxbox.ID,
+		key -> new Item.Properties().setId(key)
+	);
 
-	public static void init() {
-		register(SPEAKER, Item::new);
-		register(SQUEAKER, Item::new);
-		register(CARDBOARD, Item::new);
-	}
+	public static final RegistryContext.Value<Item> SPEAKER = CONTEXT.defer(
+		"speaker",
+		Item::new
+	);
 
-	public static ResourceKey<Item> key(String path) {
-		return ResourceKey.create(Registries.ITEM, MoreThanAFoxbox.id(path));
-	}
+	public static final RegistryContext.Value<Item> SQUEAKER = CONTEXT.defer(
+		"squeaker",
+		Item::new
+	);
 
-	private static <T extends Item> void register(ResourceKey<Item> key, Function<Item.Properties, T> func) {
-		Registry.register(BuiltInRegistries.ITEM, key, func.apply(new Item.Properties()));
+	public static final RegistryContext.Value<Item> CARDBOARD = CONTEXT.defer(
+		"cardboard",
+		Item::new
+	);
+
+	public static void register() {
+		CONTEXT.register();
 	}
 }
