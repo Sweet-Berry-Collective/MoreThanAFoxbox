f1f7d7806a43a0e519ebdeba5bd1a777a7a8b38d
Anastasia Calico <calico@merchantcalico.net>
2025-08-08 13:05:25 +1000

    feat: Create plushie model loading systems.

---
+++ src/main/java/dev/sweetberry/more_than_a_foxbox/client/MoreThanAFoxboxClient.java
@@ -0,0 +1,19 @@
+package dev.sweetberry.more_than_a_foxbox.client;
+
+import dev.sweetberry.more_than_a_foxbox.client.util.PlushieModelUtil;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.model.loading.v1.ExtraModelKey;
+import net.fabricmc.fabric.api.client.model.loading.v1.PreparableModelLoadingPlugin;
+import net.fabricmc.fabric.api.client.model.loading.v1.SimpleUnbakedExtraModel;
+import net.minecraft.resources.ResourceLocation;
+
+public class MoreThanAFoxboxClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		PreparableModelLoadingPlugin.register(PlushieModelUtil::getModels, (data, pluginContext) -> {
+			for (ResourceLocation id : data) {
+				pluginContext.addModel(ExtraModelKey.create(id::toString), SimpleUnbakedExtraModel.blockStateModel(id));
+			}
+		});
+	}
+}
---
+++ src/main/java/dev/sweetberry/more_than_a_foxbox/client/util/PlushieModelUtil.java
@@ -0,0 +1,35 @@
+package dev.sweetberry.more_than_a_foxbox.client.util;
+
+import dev.sweetberry.more_than_a_foxbox.data.PlushieVariant;
+import net.fabricmc.fabric.api.client.model.loading.v1.ExtraModelKey;
+import net.fabricmc.fabric.api.client.model.loading.v1.SimpleUnbakedExtraModel;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.BlockStateModel;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.FileToIdConverter;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+public class PlushieModelUtil {
+	public static CompletableFuture<List<ResourceLocation>> getModels(ResourceManager manager, Executor executor) {
+		FileToIdConverter fileToIdConverter = FileToIdConverter.json("models/more_than_a_fox_box");
+		return CompletableFuture.supplyAsync(() -> fileToIdConverter.listMatchingResources(manager)
+			.keySet()
+			.stream().map(resourceLocation ->
+				resourceLocation.withPath(s -> s.substring(7, s.length() - 5)))
+			.toList(), executor);
+	}
+
+	public static SimpleUnbakedExtraModel<BlockStateModel> getPlushieModel(PlushieVariant variant, ResourceLocation activePose) {
+		return Minecraft.getInstance().getModelManager().getModel(createModelKey(variant, activePose));
+	}
+
+	private static ExtraModelKey<SimpleUnbakedExtraModel<BlockStateModel>> createModelKey(PlushieVariant variant, ResourceLocation activePose) {
+		return ExtraModelKey.create(() -> variant.poses().get(activePose).model().texturePath().toString());
+	}
+}
