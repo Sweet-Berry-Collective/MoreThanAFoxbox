fcf6db2dd562aeb6ee5e2234a1be1340670bb7a9
sylv256 <contact@sylv.gay>
2025-08-08 02:22:44 -0400

    chore: apply licenses

--- src/main/java/dev/sweetberry/more_than_a_foxbox/registry/RegistryContext.java
+++ src/main/java/dev/sweetberry/more_than_a_foxbox/registry/RegistryContext.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package dev.sweetberry.more_than_a_foxbox.registry;
 
 import com.mojang.datafixers.util.Pair;
