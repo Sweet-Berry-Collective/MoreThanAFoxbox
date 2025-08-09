ee3f1b556052b5eb5f088e0fdfb8a43589d6368f
sylv256 <contact@sylv.gay>
2025-08-09 13:13:00 -0400

    fix: Temporarily remove erroneous ModelUtil methods

--- src/client/java/dev/sweetberry/more_than_a_foxbox/client/util/ModelUtil.java
+++ src/client/java/dev/sweetberry/more_than_a_foxbox/client/util/ModelUtil.java
@@ -6,34 +6,49 @@
 
 package dev.sweetberry.more_than_a_foxbox.client.util;
 
-import dev.sweetberry.more_than_a_foxbox.data.PlushieVariant;
-import net.fabricmc.fabric.api.client.model.loading.v1.ExtraModelKey;
-import net.fabricmc.fabric.api.client.model.loading.v1.SimpleUnbakedExtraModel;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.model.BlockStateModel;
+import dev.sweetberry.more_than_a_foxbox.MoreThanAFoxbox;
 import net.minecraft.resources.FileToIdConverter;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 public class ModelUtil {
-	public static CompletableFuture<List<ResourceLocation>> getModels(ResourceManager manager, Executor executor) {
-		FileToIdConverter fileToIdConverter = FileToIdConverter.json("models/more_than_a_fox_box");
-		return CompletableFuture.supplyAsync(() -> fileToIdConverter.listMatchingResources(manager)
-			.keySet()
-			.stream().map(resourceLocation ->
-				resourceLocation.withPath(s -> s.substring(7, s.length() - 5)))
-			.toList(), executor);
+	public static CompletableFuture<List<ResourceLocation>> getPlushieModels(ResourceManager manager, Executor executor) {
+		return getModels("plushie", manager, executor);
+	}
+	
+	public static CompletableFuture<List<ResourceLocation>> getBoxModels(ResourceManager manager, Executor executor) {
+		return getModels("box", manager, executor);
 	}
 
-	public static SimpleUnbakedExtraModel<BlockStateModel> getPlushieModel(PlushieVariant variant, ResourceLocation activePose) {
-		return Minecraft.getInstance().getModelManager().getModel(createModelKey(variant, activePose));
+	private static @NotNull CompletableFuture<List<ResourceLocation>> getModels(
+		String path,
+		ResourceManager manager,
+		Executor executor
+	) {
+		FileToIdConverter fileToIdConverter = FileToIdConverter.json("models/" + MoreThanAFoxbox.ID + "/" + path);
+		return CompletableFuture.supplyAsync(
+			() -> fileToIdConverter.listMatchingResources(manager)
+				.keySet()
+				.stream().map(resourceLocation ->
+					resourceLocation.withPath(s -> s.substring(
+						7,
+						s.length() - 5
+					)))
+				.toList(), executor
+		);
 	}
 
-	private static ExtraModelKey<SimpleUnbakedExtraModel<BlockStateModel>> createModelKey(PlushieVariant variant, ResourceLocation activePose) {
-		return ExtraModelKey.create(() -> variant.poses().get(activePose).model().texturePath().toString());
-	}
+	// FIXME: Update code based on new refactors
+//	public static SimpleUnbakedExtraModel<BlockStateModel> getPlushieModel(PlushieVariant variant, ResourceLocation activePose) {
+//		return Minecraft.getInstance().getModelManager().getModel(createModelKey(variant, activePose));
+//	}
+//
+//	private static ExtraModelKey<SimpleUnbakedExtraModel<BlockStateModel>> createModelKey(PlushieVariant variant, ResourceLocation activePose) {
+//		return ExtraModelKey.create(() -> variant.poses().get(activePose).model().texturePath().toString());
+//	}
 }
