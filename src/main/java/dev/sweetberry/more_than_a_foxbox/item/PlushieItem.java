6fd5c5394dbaed0dfb58733eba7f2ce42e15cb88
Oliver-makes-code <oliver@olivermakesco.de>
2025-08-11 14:47:44 -0500

    Plushie sounds!

--- src/main/java/dev/sweetberry/more_than_a_foxbox/item/PlushieItem.java
+++ src/main/java/dev/sweetberry/more_than_a_foxbox/item/PlushieItem.java
@@ -7,10 +7,21 @@
 package dev.sweetberry.more_than_a_foxbox.item;
 
 import dev.sweetberry.more_than_a_foxbox.component.MtfbComponents;
+import dev.sweetberry.more_than_a_foxbox.component.PlushieDataComponent;
+import dev.sweetberry.more_than_a_foxbox.sound.MtfbSounds;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
 
 public class PlushieItem extends Item {
 	public PlushieItem(Properties properties) {
@@ -27,5 +38,44 @@ public class PlushieItem extends Item {
 		var location = plushie.variant().location();
 
 		return Component.translatable("item.more_than_a_foxbox.plushie." + location.toLanguageKey());
+	}
+
+	public Optional<SoundEvent> getSoundEvent(RegistryAccess access, ItemStack stack) {
+		var plushie = stack.get(MtfbComponents.PLUSHIE.get());
+
+		if (plushie == null)
+			return Optional.empty();
+
+		var maybeSoundType = plushie.soundType();
+
+		if (maybeSoundType.isEmpty())
+			return Optional.empty();
+
+		var soundType = maybeSoundType.get();
+
+		if (soundType == PlushieDataComponent.SoundType.SPEAKER) {
+			var maybeVariant = access.get(plushie.variant());
+
+			return maybeVariant.map(
+				plushieVariantReference -> plushieVariantReference
+					.value()
+					.mobSounds()
+					.value()
+			);
+		}
+
+		return Optional.of(MtfbSounds.SQUEAK.get());
+	}
+
+	@Override
+	public @NotNull InteractionResult use(Level level, Player player, InteractionHand hand) {
+		var sound = getSoundEvent(level.registryAccess(), player.getItemInHand(hand));
+
+		if (sound.isEmpty())
+			return super.use(level, player, hand);
+
+		level.playPlayerSound(sound.get(), SoundSource.PLAYERS, 1, 1);
+
+		return InteractionResult.SUCCESS;
 	}
 }
---
+++ src/main/java/dev/sweetberry/more_than_a_foxbox/sound/MtfbSounds.java
@@ -0,0 +1,35 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package dev.sweetberry.more_than_a_foxbox.sound;
+
+import dev.sweetberry.more_than_a_foxbox.MoreThanAFoxbox;
+import dev.sweetberry.more_than_a_foxbox.registry.RegistryContext;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.sounds.SoundEvent;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public final class MtfbSounds {
+	private static final RegistryContext<SoundEvent> CONTEXT = new RegistryContext<>(BuiltInRegistries.SOUND_EVENT, MoreThanAFoxbox.ID);
+
+	public static final Supplier<SoundEvent> SQUEAK = CONTEXT.defer(
+		"plushie.squeak",
+		withVariableRange()
+	);
+
+	private MtfbSounds() {}
+
+	public static Function<ResourceKey<SoundEvent>, SoundEvent> withVariableRange() {
+		return key -> SoundEvent.createVariableRangeEvent(key.location());
+	}
+
+	public static void register() {
+		CONTEXT.register();
+	}
+}
