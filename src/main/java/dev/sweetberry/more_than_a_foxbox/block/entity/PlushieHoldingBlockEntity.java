f707bc54b89ba41216f7bc0dd74f5d1c16ab73ac
sylv256 <contact@sylv.gay>
2025-08-11 19:28:51 -0400

    fix: synchronize Plushie Block Entity variant

--- src/main/java/dev/sweetberry/more_than_a_foxbox/block/entity/PlushieHoldingBlockEntity.java
+++ src/main/java/dev/sweetberry/more_than_a_foxbox/block/entity/PlushieHoldingBlockEntity.java
@@ -9,17 +9,27 @@ package dev.sweetberry.more_than_a_foxbox.block.entity;
 import dev.sweetberry.more_than_a_foxbox.component.MtfbComponents;
 import dev.sweetberry.more_than_a_foxbox.component.PlushieDataComponent;
 import dev.sweetberry.more_than_a_foxbox.data.PlushieVariant;
+import dev.sweetberry.more_than_a_foxbox.registry.MtfbRegistries;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
 public abstract class PlushieHoldingBlockEntity extends BlockEntity {
+	private ResourceLocation variant;
+
 	public PlushieHoldingBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
 		super(type, pos, blockState);
 	}
@@ -39,4 +49,36 @@ public abstract class PlushieHoldingBlockEntity extends BlockEntity {
 	}
 
 	public abstract Optional<ResourceLocation> getPoseModel(BlockState state);
+
+	@Override
+	public void setLevel(Level level) {
+		super.setLevel(level);
+		if (this.getLevel() != null && this.variant != null) {
+			RegistryAccess registryAccess = this.getLevel().registryAccess();
+			registryAccess.lookup(MtfbRegistries.PLUSHIE_VARIANT)
+				.flatMap(
+					registry -> registry.get(this.variant)
+				)
+				.ifPresent(this::setPlushieVariant);
+		}
+	}
+
+	@Override
+	protected void loadAdditional(ValueInput input) {
+		input.getString("variant")
+			.ifPresent(variant -> this.variant = ResourceLocation.parse(variant));
+	}
+
+	@Override
+	protected void saveAdditional(ValueOutput output) {
+		if (this.components().has(MtfbComponents.PLUSHIE.get())) {
+			@SuppressWarnings("DataFlowIssue") ResourceLocation variant = this.components().get(MtfbComponents.PLUSHIE.get()).variant().unwrapKey().orElseThrow().location();
+			output.putString("variant", variant.toString());
+		}
+	}
+
+	@Override
+	public @NotNull CompoundTag getUpdateTag(HolderLookup.Provider registries) {
+		return this.saveWithoutMetadata(registries);
+	}
 }
